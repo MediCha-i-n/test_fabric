de66d4741c312a5b219c443b926e896f24eeebb1
bm1478 <bm1478@naver.com>
2020-09-03 22:03:44 +0900

    add LICENSE

--- requisitesInstall.sh
+++ requisitesInstall.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-cache search docker-ce
-apt-get install -y docker-ce
+sudo apt-get install -y docker-ce
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
