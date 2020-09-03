db34f2088752baa580a55013d287ca9ab469f8f1
beoms <bm1478@naver.com>
2020-09-03 21:57:54 +0900

    fix install shell script

--- requisitesInstall.sh
+++ requisitesInstall.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
+sudo apt-get update
 sudo apt-get install build-essential
 
 sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update && sudo apt-get dist-upgrade
-sudo apt-get install git-core
+sudo apt-get dist-upgrade
+sudo apt-get install -y git-core
 
 sudo apt-get install -y curl
 
-sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update && sudo apt-cache search docker-ce
-sudo apt-get update && sudo apt-get install docker-ce
-sudo usermod -aG docker $USER
-sudo service docker restart
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo apt-cache search docker-ce
+apt-get install -y docker-ce
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
