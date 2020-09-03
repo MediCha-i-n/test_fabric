ea375bcd52f3bab9cf6ab210eb9969630bde0957
beoms <bm1478@naver.com>
2020-09-03 21:42:38 +0900

    add install shell script

---
+++ requisitesInstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get install build-essential
+
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install git-core
+
+sudo apt-get install -y curl
+
+apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update && sudo apt-cache search docker-ce
+sudo apt-get update && sudo apt-get install docker-ce
+sudo usermod -aG docker $USER
+sudo service docker restart
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
