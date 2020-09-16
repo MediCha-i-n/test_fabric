3d17645a52da93c16a24f829ee78068512c327d4
beoms <bm1478@naver.com>
2020-09-16 10:19:06 +0900

    add my ipfs service

---
+++ test_ipfs/imgAdder.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const IpfsHttpClient = require('ipfs-http-client')  // need a IPFS Daemon
+
+const ipfs = IpfsHttpClient()
+
+async function addipfs(filename){
+    const addOptions = {
+        timeout: 10000
+    }
+
+    try{
+        return await ipfs.add(fs.createReadStream(filename), addOptions);
+    } catch(err) {
+        console.log(err)
+    }
+    return false;
+}
+
+addipfs('./sampleImages/original/1.tif').then((result) => {
+    console.log(result);
+})
+
+addipfs('./sampleImages/ground-truth/1.tif').then((result) => {
+    console.log(result);
+})
---
+++ test_ipfs/imgGetter.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const IpfsHttpClient = require('ipfs-http-client')
+
+const ipfs = IpfsHttpClient()
+
+async function getipfs(cid, filename){
+    for await (const file of ipfs.get(cid)) {
+        console.log(file);
+        if (!file.content) continue;
+
+        const content = []
+
+        for await (const chunk of file.content) {
+            content.push(chunk)
+        }
+
+        const buf = Buffer.concat(content);
+
+        fs.writeFileSync(filename, buf, 'base64')
+        return buf;
+    }
+}
+
+const cidRaw = 'QmVWn1q82hNNJSttYgNAhhHE6iMdcTHS2VF6C2zTTbKg6o';
+const cidResult = 'QmeSSVxj2qozvTQAQWhnCcsq6J8HAUmTV14LvWrdRps8Wn';
+
+getipfs(cidRaw, './result/ipfs_raw.tif').then(console.log);
+getipfs(cidResult, './result/ipfs_origin.tif').then(console.log);
